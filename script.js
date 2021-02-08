f5aa2ce135c3dfd9f293c3c09b3470f09cbf6b22
Pipes <nolanjohnclark@gmail.com>
2021-02-08 15:17:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('click', musicPlay);
+function musicPlay() {
+    document.getElementById('player').play();
+    document.removeEventListener('click', musicPlay);
+}
+document.addEventListener('wheel', musicPlay);
+function musicPlay() {
+    document.getElementById('player').play();
+    document.removeEventListener('wheel', musicPlay);
+}
